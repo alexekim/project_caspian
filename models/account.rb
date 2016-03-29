17e548259adc061310390ab9f5da1797818cc866
Max Stern <stern.maxwell@gmail.com>
2016-03-29 15:05:43 -0500

    minor changes to account model

--- models/account.rb
+++ models/account.rb
@@ -1,3 +1,20 @@
 class Account < ActiveRecord::Base
 
+  #password setter/mutator
+  # def password = (users_password)
+  #   self.password_hash = BCrypt::Password.create(users_password)
+  # end
+  #
+  # def has_valid_password(user_id, attempted_password)
+  #   @current_user = Account.find(user_id)
+  #   comparison = BCrypt::Password.new(attempted_password)
+  #   if (@current_user[:password_hash].to_s) == comparison.to_s)
+  #     return true
+  #   else
+  #     return false
+  #   end
+  # end
+
+  #password getter
+
 end
