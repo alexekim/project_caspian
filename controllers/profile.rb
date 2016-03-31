3e6cf82c4469e46a1ac20dfcb9fea2dc9d94c1a6
Alex Kim <alexander.e.kim@gmail.com>
2016-03-31 15:44:34 -0500

    changes

--- controllers/profile.rb
+++ controllers/profile.rb
@@ -1,6 +1,6 @@
 class ProfileController < ApplicationController
   get '/' do
-      
+
       # p session[:personal]
       # @personal_info = Personal.find_by(account_id: session[:user_id])
       # @contact_info  = Contact.find_by(account_id: session[:user_id])
@@ -9,8 +9,6 @@ class ProfileController < ApplicationController
 
       @account = Account.find session[:user_id]
       # @account.contact_info.address
-
-
 
       erb :profile, locals: {title: 'User Profile'}
   end
